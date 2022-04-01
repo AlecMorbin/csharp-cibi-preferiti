ea4cc916e7f08fbb147fe05b33969974b9705980
Alec <alemor98@hotmail.it>
2022-04-01 12:59:23 +0200

    Add project files.

---
+++ CibiPreferiti/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] cibiPreferiti = { "Pasticcio", "Spaghetti", "Mille foglie","Costicine con salsa barbecue" };
+
+Console.WriteLine("La lunghezza della classifica è: "+cibiPreferiti.Length);
+
+foreach (string cibo in cibiPreferiti)
+{
+    Console.WriteLine(cibo);
+}
+
+Console.WriteLine(cibiPreferiti[0]);
+
+Console.WriteLine(cibiPreferiti[cibiPreferiti.Length-1]);
