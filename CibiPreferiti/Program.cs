61cb1715c12d488d7451f4ae3ccc1909a8bbad73
Alec <alemor98@hotmail.it>
2022-04-01 13:05:55 +0200

    Migliorie al programma

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,12 +1,15 @@
-﻿string[] cibiPreferiti = { "Pasticcio", "Spaghetti", "Mille foglie","Costicine con salsa barbecue" };
+﻿string[] cibiPreferiti = { "Pasticcio", "Spaghetti", "Mille foglie","Costicine con salsa barbecue" , "Pollo fritto speziato" };
 
 Console.WriteLine("La lunghezza della classifica è: "+cibiPreferiti.Length);
 
+Console.WriteLine("\nLa lista dei cibi é: ");
 foreach (string cibo in cibiPreferiti)
 {
     Console.WriteLine(cibo);
 }
 
+Console.WriteLine("\nIl mio cibo preferito è: ");
 Console.WriteLine(cibiPreferiti[0]);
 
+Console.WriteLine("\nIl mio cibo preferito ma non tanto quanto gli altri è: ");
 Console.WriteLine(cibiPreferiti[cibiPreferiti.Length-1]);
